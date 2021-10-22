3528a0c9e9f8843d98e39d4c0b38c1e40b033611
Jared Shakespaere <jaredshakespaere@Jareds-MacBook-Pro.local>
2021-10-22 12:34:57 -0500

    first-commit-full-assessment

---
+++ tictactoe.test.ts
@@ -0,0 +1,50 @@
+import { Builder, Capabilities, By } from "selenium-webdriver"
+
+const chromedriver = require('chromedriver')
+
+const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
+
+beforeAll(async () => {
+    await driver.get('http://localhost:4000')
+})
+
+afterAll(async () => {
+    await driver.quit()
+})
+
+describe('tic tac toe game', async () => {
+test('I can start a game', async () => {
+    
+    let button = await (await driver).findElement(By.id('start-game'));
+    await button.click();
+    
+    await driver.sleep(2000)
+}),
+
+test('Play top left square', async () => {
+    
+    let leftSquare = await (await driver).findElement(By.id("cell-0"));
+    await leftSquare.click();
+    
+    await driver.sleep(2000)
+    
+}),
+
+test('Play top right square', async () => {
+
+    let rightSquare = await (await driver).findElement(By.id("cell-2"));
+    await rightSquare.click();
+
+    await driver.sleep(2000)
+   
+}),
+
+test('Play lower right square', async () => {
+
+    let lowerRightSquare = await (await driver).findElement(By.id("cell-8"));
+    await lowerRightSquare.click();
+    
+    await driver.sleep(2000)
+ 
+});
+});
